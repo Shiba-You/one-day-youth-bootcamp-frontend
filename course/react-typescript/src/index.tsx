d08e250ba632816138fab85dc811f10cd868b59b
Shiba-You <50415577+Shiba-You@users.noreply.github.com>
2021-12-13 16:38:17 +0900

    first commit

--- course/react-typescript/src/index.tsx
+++ course/react-typescript/src/index.tsx
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { request } from "./server";
 import { TaskList } from './components/TaskList';
 import { TaskForm } from './components/TaskForm';
 
 // TODOタスクの型
-export type Task = {}
+export type Task = {
+  label: string;
+  isDone: boolean
+}
 
 const App: React.VFC = () => {
   // タスクリストを格納する
-
+  const [tasks, setTasks] = useState<Task[]>([]);
   // 追加前のタスクを格納する
-
+  const [newTaskLabel, setNewTaskLabel] = useState<string>('');
   // ページマウント時にモックAPIからデータを取得
+  useEffect(() => {
+    request.fetchTasks((payload: Task[]) => {
+      setTasks(payload);
+    });
+  }, []);
 
+  const check = () => {
+    console.log(tasks)
+  }
+  
   return (
     <div>
       {/* ヘッダー */}
-      <h1>Tutorial Works</h1>
+      <h1 style={{ color: 'red' }}>Tutorial Works</h1>
+      <p>This is Cyber Agent Handson!!</p>
 
       {/* 一覧表示 */}
-      <TaskList />
+      {/* TODO: ここはFragmentを省略している */}
+      <TaskList {...{ tasks, setTasks}} />
 
       {/* タスク追加、削除 */}
-      <TaskForm />
+      <TaskForm {...{ tasks, setTasks, newTaskLabel, setNewTaskLabel }} />
+      <button 
+        onClick={check}
+      >
+        check
+        </button>
     </div>
   );
 };
